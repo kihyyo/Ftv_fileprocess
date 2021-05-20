401a61ac235781e45b53b6f136cdde6699f0bc8f
kihyyo <83176214+kihyyo@users.noreply.github.com>
2021-05-20 23:29:17 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
 from .plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info
 from .logic import Logic
+try:
+    from guessit import guessit
+except:
+    try:
+        os.system("{} install guessit".format(app.config['config']['pip']))
+        from guessit import guessit
+    except:
+        pass
